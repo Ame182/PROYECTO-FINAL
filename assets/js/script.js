de4ef1edc47f24052574ec8eaa771e892fbcca70
Amelia Fayad <ame@MacBook-Pro-de-Amelia.local>
2019-01-16 20:18:59 -0300

    footer con iframe

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,13 @@
  $(function(){
-      $('#galeria-fotos , img').click(function(){
-          $('.satura').not(this).removeClass('satura');
-          $(this).toggleClass('satura');
+    $('.cero').click(function(){
+        $('.satura').not(this).removeClass('satura');
+        $(this).toggleClass('satura');
+    });
 
-      })
-    })
+
+    $('#register_button').click(function(e){
+    	alert('Ingresa tus datos');
+    	e.preventDefault();
+  	})  	
+
+ })
